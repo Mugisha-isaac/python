323804332e0d94b7ca14d337fb6442d482418f68
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-01-26 19:21:17 +0200

    tuples

--- tuples.py
+++ tuples.py
@@ -11,19 +11,19 @@ num_tuple=(1,2,3,4,5)
 # add = (3+5,)
 # print(add*2)
 # print(num[2:])
-num_list = [1,2,3,4,5]
-num_list[1]=9
-print(num_list)
+# num_list = [1,2,3,4,5]
+# num_list[1]=9
+# print(num_list)
 
-fruits = ("apple","banana","mango")
-print('orange' in fruits)
+# fruits = ("apple","banana","mango")
+# print('orange' in fruits)
 
-x=5
-y=7
-x,y=y,x
+# x=5
+# y=7
+# x,y=y,x
 
-print("x is = ",x)
-print("y is = ",y)
+# print("x is = ",x)
+# print("y is = ",y)
 
 #assignment
 # email is mugishaisaac@gmail.com
@@ -37,7 +37,40 @@ print("y is = ",y)
 # print("username is: ",username)
 # print("domain is: ",domain)
 
-email = "mugishaisaac@gmail.com"
-username,domain = email.split('@')
-print('username: ', username)
-print('domain: ', domain)
+# email = "mugishaisaac@gmail.com"
+# username,domain = email.split('@')
+# print('username: ', username)
+# print('domain: ', domain)
+
+
+# def min_max(t):
+#     return min(t),max(t)
+# t=(1,2,3,4,56,7)
+
+# minimum,maximum = min_max(t)
+# print(minimum)
+# print(maximum)
+
+# num=[1,2,3]
+# names =['isaac','mugisha','doe']
+# print(zip(num,names))
+
+# matrix = ((1,2,3),(4,5,6),(7,8,9))
+# print(matrix[0][0])
+
+# t=1,2,3,4,5,6,7
+# for i in t:
+#     print(i)
+
+
+# marged_list = [(1,2,3),('isaac',4.5,8)]    
+# for first,second,third in marged_list:
+#     print(first)
+#     print(second)
+#     print(third)
+
+name='isaac'
+print(enumerate(name))
+for index,value in enumerate(name):
+    print('index is :',str(index) + "value is ",value)
+   
