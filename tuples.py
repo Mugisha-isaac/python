e7313abab41d96c31daf51baceb5b82ceb5309a3
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-01-26 18:55:36 +0200

    assignment 2

--- tuples.py
+++ tuples.py
@@ -1,9 +1,39 @@
 # t=1,2,'isaac',4.5
 # print(type(t))
 
-blank_tuple= tuple()
-print(blank_tuple)
+# blank_tuple= tuple()
+# print(blank_tuple)
 
-num=(1,2,3,4,5)
-print(num[2])
-print(num[-6])
+num_tuple=(1,2,3,4,5)
+# print(num[2])
+# print(num[-6])
+
+# add = (3+5,)
+# print(add*2)
+# print(num[2:])
+num_list = [1,2,3,4,5]
+num_list[1]=9
+print(num_list)
+
+fruits = ("apple","banana","mango")
+print('orange' in fruits)
+
+x=5
+y=7
+x,y=y,x
+
+print("x is = ",x)
+print("y is = ",y)
+
+#assignment
+# email is mugishaisaac@gmail.com
+# extract username and domain from gmail
+# username dshould be mugishaisaac
+# domain should be gmail.com
+
+email = 'mugishaisaac@gmail.com'
+username = email[0:12]
+domain = email[13:]
+print("username is: ",username)
+print("domain is: ",domain)
+
